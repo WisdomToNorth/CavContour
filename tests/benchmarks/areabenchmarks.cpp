20f18bc2d9b878dd578dc40507693a1820ec2617
KYC_HP_Slim_wsl <kangyucheng1998@126.com>
2023-12-22 20:02:40 +0800

    init project

---
+++ tests/benchmarks/areabenchmarks.cpp
@@ -0,0 +1,13 @@
+#include <benchmark/benchmark.h>
+
+#include "benchmarkprofiles.h"
+#include "cavc/polyline.hpp"
+
+static void area(NoSetup, TestProfile const &profile)
+{
+    cavc::getArea(profile.pline);
+}
+CAVC_CREATE_BENCHMARKS(area, NoSetup, area, benchmark::kNanosecond)
+CAVC_CREATE_NO_ARCS_BENCHMARKS(area, NoSetup, area, 0.01, benchmark::kNanosecond)
+
+BENCHMARK_MAIN();
---
+++ tests/benchmarks/extentsbenchmarks.cpp
@@ -0,0 +1,13 @@
+#include <benchmark/benchmark.h>
+
+#include "benchmarkprofiles.h"
+#include "cavc/polyline.hpp"
+
+static void extents(NoSetup, TestProfile const &profile)
+{
+    cavc::getExtents(profile.pline);
+}
+CAVC_CREATE_BENCHMARKS(extents, NoSetup, extents, benchmark::kNanosecond)
+CAVC_CREATE_NO_ARCS_BENCHMARKS(extents, NoSetup, extents, 0.01, benchmark::kNanosecond)
+
+BENCHMARK_MAIN();
